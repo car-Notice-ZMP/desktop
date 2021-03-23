fd868656c47b8e3ca0d0a8af26ea45aa88289b9a
Robert Klinger <79660078+Eater0@users.noreply.github.com>
2021-03-23 17:07:55 +0100

    step 2

--- Models/Login/ILoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Models.Login
-{
-    interface ILoginModel
-    {
-        string email { get; set; }
-        string password { get; set; }
-    }
-}
---
+++ NoticeMyCar/Views/IFormLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NoticeMyCar.Views
+{
+    /*public interface IReport
+    {
+        void Report(bool report);
+    }*/
+
+    public interface IFormLogin
+    {
+        string email { get; }
+        string password { get; }
+
+        event EventHandler LoginEvent;
+
+        void Report(bool report);
+    }
+}
