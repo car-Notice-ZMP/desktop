0dfd10a77e8344cdad25c83441b368933805d51e
Robert Klinger <79660078+Eater0@users.noreply.github.com>
2021-05-11 16:11:33 +0200

    change name project Test

--- Test/Properties/AssemblyInfo.cs
+++ Test/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProjectNoticeMyCar")]
+[assembly: AssemblyTitle("Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProjectNoticeMyCar")]
+[assembly: AssemblyProduct("Test")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("ba64bd0f-f320-4707-b661-b9a264b4fca2")]
+[assembly: Guid("ab7a3ec2-82fb-4c54-b82f-34c03b5d71e5")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ Test/SpaceForAnnouncements.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test
+{
+    [TestClass]
+    public class SpaceForAnnouncements
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
